2d75a5dd6b23d10dc924a7efe06bf76aa76b15ae
mabrursatori <mabrursatori@gmail.com>
2022-06-29 23:38:47 +0700

    theme settings

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ const App = () => {
                 </div>
            
             <div>
+                <ThemeSettings/>
                 <Routes>
                     {/* Dashboard */}
                     <Route path='/' element={<Ecommerce/>}/>
